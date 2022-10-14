43c6d7e940b51fde8f5d35449a6ee179b0c3ee64
André Nunes <andre.n@edu.sc.senai.br>
2022-10-14 19:57:08 -0300

    aula 4

---
+++ aula4/src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+    public static Double somar(Double num1, Double num2){
+        return num1+num2;
+    }
+
+    public static Integer somar(Integer num1, Integer num2){
+        return num1+num2;
+    }
+}
